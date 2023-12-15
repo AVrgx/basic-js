dd756269c5d5b1ff670075dc89853be6ca77c9eb
AVrgx <avargx@gmail.com>
2023-12-15 17:26:01 +0900

    feat: commomn character done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const {
+    repeatTimes = 1,
+    separator = "+",
+    addition = "",
+    additionRepeatTimes = 1,
+    additionSeparator = "|"
+  } = options
+
+  let additionEnd = new Array(additionRepeatTimes).fill(addition).join(additionSeparator)
+  let res = new Array(repeatTimes).fill(str + additionEnd).join(separator)
+  return res
 }
 
 module.exports = {
